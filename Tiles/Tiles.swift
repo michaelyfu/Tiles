343e5d699c962627f59d87c61d03926a7df8e2db
RushilShaw <shah.rushil.s@gmail.com>
2021-04-16 18:49:14 -0400

    test10

--- Tiles/Tiles.swift
+++ Tiles/Tiles.swift
@@ -15,7 +15,7 @@ func myMain() -> String
     
     var tempClient = TileClient()
     
-    tempClient.onBoard[4][4] = "T"
+    //tempClient.onBoard[4][4] = "T"
     tempClient.onBoard[4][5] = "H"
     tempClient.onBoard[4][6] = "I"
     tempClient.onBoard[4][7] = "S"
@@ -26,8 +26,8 @@ func myMain() -> String
     tempClient.onBoard[7][6] = "L"
     tempClient.onBoard[7][7] = "L"
     tempClient.onBoard[8][7] = "O"
-    //tempClient.onBoard[9][7] = "W"
-    //tempClient.onBoard[10][7] = "X"
+    tempClient.onBoard[9][7] = "W"
+    tempClient.onBoard[10][7] = "X"
     
     return String(tempClient.callSplit())
 }
